0d1fe06699910bea8648b078ce0407a47b2b8daa
Fran Aguilera <fraguile@microsoft.com>
2022-04-01 14:44:48 -0400

    Initial commit.

--- DeployTPMEFLOW.ps1
+++ DeployTPMEFLOW.ps1
@@ -13,23 +13,22 @@ if (-Not (Verify-EflowVm)) {
     exit 1
 }
 Write-Host "EFLOW installed. Building solution..." -ForegroundColor "green"
-dotnet build "tpm-read-nv.sln" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=linux-x64
-if($LASTEXITCODE -ne 0)
-{
+dotnet build "tpm-read-nv\tpm-read-nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=linux-x64
+dotnet build "tpm-write-nv\tpm-write-nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=win-x64
+if ($LASTEXITCODE -ne 0) {
     Write-Host "Error building solution." -ForegroundColor "red"
     exit $LASTEXITCODE
 }
 Write-Host "Tar'ing binaries..." -ForegroundColor "green"
-tar -C .\tpm-read-nv\bin\Release\net6.0 -cvf tpm-read-nv.tar linux-x64
-if($LASTEXITCODE -ne 0)
-{
+tar -C ".\tpm-read-nv\bin\Release\net6.0" -cf "tpm-read-nv.tar" linux-x64
+tar -C ".\tpm-write-nv\bin\Release\net6.0" -cf "tpm-write-nv.tar" win-x64
+if ($LASTEXITCODE -ne 0) {
     Write-Host "Error tar'ing binaries." -ForegroundColor "red"
     exit $LASTEXITCODE
 }
 Write-Host "Copying binaries to EFLOW..." -ForegroundColor "green"
-Copy-EflowVmFile -fromFile tpm-read-nv.tar -toFile ~/tpm-read-nv.tar -pushFile
-if($LASTEXITCODE -ne 0)
-{
+Copy-EflowVmFile -fromFile "tpm-read-nv.tar" -toFile "~/tpm-read-nv.tar" -pushFile
+if ($LASTEXITCODE -ne 0) {
     Write-Host "Error copying binaries to EFLOW." -ForegroundColor "red"
     exit $LASTEXITCODE
 }
--- README.md
+++ README.md
@@ -8,8 +8,31 @@ https://docs.microsoft.com/en-us/azure/iot-edge/how-to-provision-single-device-l
 
 Modify EFLOW ps1 to allow TPM2
 
-Add the `-IncludeHidden` flag to all of the `Get-VMNetworkAdapter` and `Get-NetAdapter` commandlets in:
+Add the `-IncludeHidden` flag to all of the `Get-NetAdapter` commandlets in:
 C:\Program Files\WindowsPowerShell\Modules\AzureEFLOW\AzureEFLOW.psm1:3202-3228
+
+Remove-Module AzureEFLOW
+Import-Module AzureEFLOW
+Start-EflowVm
+Set-EflowVmFeature -feature 'DpsTpm' -Enable
+
+https://github.com/microsoft/TSS.MSR/tree/master/TSS.NET/Samples/NV%20(Windows)
+openssh -> generate key
+use cryptsetup to encrypt partition create filsystem...
+use encryption key to decrypt partition
+
+cryptsetup -c aes-xts-plain64 --key-size 512 --hash sha512 --time 5000 --use-urandom /dev/sdb1
+cryptsetup open /dev/sdb1 encrypted
+
+cryptsetup luksFormat --type luks2 /dev/sda10
+cryptsetup luksOpen /dev/sda10 datadrive
+
+echo -e "n\n10\n\n\nw" | sudo fdisk /dev/sda
+sudo cryptsetup luksFormat /dev/sda10
+
+1. Take key we set up partition with cryptsetup.
+2. Write that key from windows with NV R/W tool.
+3. Read that key from the TPM from linux and use it to decrypt partition.
 
 ### .NET Core 6
 
