b152541a56b073e25bdb0f85185595a7b8fe39c7
Fran Aguilera <fraguile@microsoft.com>
2022-04-04 23:29:11 -0400

    Updated script.

--- BuildAndDeployNV.ps1
+++ BuildAndDeployNV.ps1
@@ -13,20 +13,20 @@ if (-Not (Verify-EflowVm)) {
     exit 1
 }
 Write-Host "EFLOW installed. Building solution..." -ForegroundColor "green"
-dotnet build "tpm-read-nv\tpm-read-nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=linux-x64
-dotnet build "tpm-write-nv\tpm-write-nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=win-x64
+dotnet build "nv\nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=linux-x64
+dotnet build "nv\nv.csproj" -c "Release" -p:DeployOnBuild=true -p:MyRuntimeIdentifier=win-x64
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Error building solution." -ForegroundColor "red"
     exit $LASTEXITCODE
 }
 Write-Host "Tar'ing binaries..." -ForegroundColor "green"
-tar -C ".\tpm-read-nv\bin\Release\net6.0" -cf "tpm-read-nv.tar" linux-x64
+tar -C ".\nv\bin\Release\net6.0" -cf "nv.tar" linux-x64
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Error tar'ing binaries." -ForegroundColor "red"
     exit $LASTEXITCODE
 }
-Write-Host "Copying binaries to EFLOW..." -ForegroundColor "green"
-Copy-EflowVmFile -fromFile "tpm-read-nv.tar" -toFile "~/tpm-read-nv.tar" -pushFile
+Write-Host "Copying binaries to EFLOW home directory..." -ForegroundColor "green"
+Copy-EflowVmFile -fromFile "nv.tar" -toFile "~/nv.tar" -pushFile
 if ($LASTEXITCODE -ne 0) {
     Write-Host "Error copying binaries to EFLOW." -ForegroundColor "red"
     exit $LASTEXITCODE
